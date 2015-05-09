442515521a3448de7ac60a468f9b4a344f474ef0
Paspartout <paspartout@fogglabs.de>
2015-05-09 13:51:28 +0200

    read max backlight brightness from device, refactoring

--- blctl.c
+++ blctl.c
@@ -31,19 +31,37 @@ read_ambient_light()
 }
 
 static int
-map_brightness(int br)
+map_brightness(int br, int max_br)
 {
-	const double factor = ((double)max_br_value/(double)(max_am_value));
-	int calc_val = (int) ceil( factor * br ) + min_brightness;
-	if(calc_val > max_br_value)
-		calc_val = max_br_value;
+	const int min_br = (int) floor(((double)min_brightness/100.0 * (double)max_br));
+	const double factor = ((double)max_br/(double)(max_am_value));
+	int calc_val = (int) floor( factor * br ) + min_br;
+
+	if(calc_val > max_br)
+		calc_val = max_br;
+
 	return calc_val;
 }
 
-void
-set_brightness(int br)
+static int
+read_max_brightness()
 {
-	FILE *f = fopen(screen_brightness_path, "w");
+	int max_br = 0;
+	FILE *f = fopen(bl_max_br_path, "r");
+
+	if(f == NULL)
+		err("can't open screen brightness file to write");
+	if (fscanf(f, "%d", &max_br) < 1)
+		err("can't write new brightness");
+
+	fclose(f);
+	return max_br;
+}
+
+static void
+set_backlight(int br)
+{
+	FILE *f = fopen(bl_br_path, "w");
 	if(f == NULL)
 		err("can't open screen brightness file to write");
 	if (fprintf(f, "%d", br) < 1)
@@ -56,13 +74,14 @@ main(int argc, char *argv[])
 {
 	int amb_light = 0;
 	int new_br = 0;
+	const int max_br = read_max_brightness();
 
 	for (;;) {
 		amb_light = read_ambient_light();
 
-		new_br = map_brightness(amb_light);
+		new_br = map_brightness(amb_light, max_br);
 
-		set_brightness(new_br);
+		set_backlight(new_br);
 
 		sleep(1);
 	}
--- config.h
+++ config.h
@@ -1,7 +1,16 @@
-static const char *am_sensor_path = "/sys/devices/platform/applesmc.768/light";
-static const char *screen_brightness_path = "/sys/class/backlight/acpi_video0/brightness";
-
+/* configs for amient light sensor */
+#define am_sensor_path "/sys/devices/platform/applesmc.768/light"
 static const int max_am_value = 255;
-static const int max_br_value = 15;
 
-static const int min_brightness = 3;
+/* configs for backlight device */
+#define backlight_path "/sys/class/backlight/acpi_video0"
+
+/* you usually don't have to modify the following two defines */
+#define bl_max_br_path backlight_path "/max_brightness"
+#define bl_br_path backlight_path "/brightness"
+
+/* defines the desired minimum brightness(brightness boost) in percent */
+static const int min_brightness = 15;
+
+/* update rate in seconds */
+static const int update_rate = 1;
